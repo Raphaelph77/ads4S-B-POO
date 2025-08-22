cbb34dbf9a6db57ac668127a6158f093044afa03
unknown <raphaelzzz72@gmail.com>
2025-08-21 22:10:55 -0300

    Projeto Criado

---
+++ src/test/java/com/ph/Fut/FutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.Fut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
